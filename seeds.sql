80177c156e7556fdd0535859879e460c20ce8f11
joshtecx <32961912+joshtecx@users.noreply.github.com>
2018-05-10 19:17:33 -0400

    updated with schema and seeds sql

---
+++ seeds.sql
@@ -0,0 +1,14 @@
+USE bamazon_db;
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES 
+("Onion", "Vegetable", 9.99, 1),
+("Pure Blue Bag", "Accessories", 99.99, 5),
+("Coke", "Soda", 1.99, 10),
+("IPhone", "Technology", 999.99, 17),
+("T-Shirt", "Clothing", 9.99, 4),
+("Headphones", "Tehnology", 49.99, 20),
+("Nintendo", "Technology", 209.99, 3),
+("Hat", "Clothing", 9.99, 16),
+("Footscapes", "Clothing", 79.99, 40),
+("Computer Case", "Technology", 19.99, 78);
